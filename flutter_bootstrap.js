975f743de0f048f076ab0bd6f15589d7c1af3b91
muhammedfaizal008 <muhammedfaizal43@gmail.com>
2025-02-17 14:19:03 +0530

    change resume link

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4026340886"
+    serviceWorkerVersion: "2868144575"
   }
 });
